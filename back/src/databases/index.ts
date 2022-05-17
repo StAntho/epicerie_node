a7babe9d938fb2c365ffe24be4c8b8d029140908
Anthony <anthony.stanix@gmail.com>
2022-05-17 17:33:20 +0200

    connection db

--- back/src/databases/index.ts
+++ back/src/databases/index.ts
@@ -4,8 +4,10 @@ import { dbConfig } from '~interfaces/db.interface';
 // const { host, port, database }: dbConfig = config.get('dbConfig');
 const { DATABASE_USER, DATABASE_PASSWORD } = process.env;
 
+const dbname = 'epicerie';
+
 export const dbConnection = {
-  url: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@clustername.f5klj.mongodb.net/database?retryWrites=true&w=majority`,
+  url: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@cluster0.hxdjv.mongodb.net/epicerie?retryWrites=true&w=majority`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
